a8bce59af714bca1dab7e4220f118e5bc6c2750f
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-10 21:49:35 +0700

    fix: styling

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -38,7 +38,7 @@ const LoginPage = () => {
                   type="text"
                   required
                   placeholder="Username"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400"
                 />
               </div>
             </div>
@@ -52,7 +52,7 @@ const LoginPage = () => {
                   type="password"
                   placeholder="Password"
                   required
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400"
                 />
               </div>
             </div>
@@ -60,7 +60,7 @@ const LoginPage = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text- font-semibold text-white drop-shadow-xl hover:drop-shadow-none hover:shadow-inner"
               >
                 Login
               </button>

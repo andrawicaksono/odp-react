31720e6aeb1a1f1aecec811b9264931c0714b4e6
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-10 16:26:42 +0700

    fix: avoid props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,6 @@ import { useEffect, useState } from "react";
 import { formatter } from "./helper/balanceFormatter";
 
 function App() {
-  const [account, setAccount] = useState({});
-  const [transactions, setTransactions] = useState([]);
-
-  useEffect(() => {
-    const fetchAccount = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/balance");
-        if (!response.ok) throw new Error("Failed to fetch");
-        const data = await response.json();
-        setAccount(data);
-      } catch (err) {
-        // alert(err.message);
-      }
-    };
-
-    const fetchTransactions = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/transactions");
-        if (!response.ok) throw new Error("Failed to fetch");
-        const data = await response.json();
-        setTransactions(data);
-      } catch (err) {
-        // alert(err.message);
-      }
-    };
-
-    fetchAccount();
-    fetchTransactions();
-  }, []);
-
   return (
     <div>
       <Navbar />

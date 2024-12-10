55399506c3e6bc1eaddbc82da80962492ea3715d
andrawicaksono <andrawicaksono14@gmail.com>
2024-12-10 16:26:58 +0700

    fix: avoid props drilling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
 import Hero from "./components/Hero";
-import photo from "./assets/photo.jpeg";
 import AccountStats from "./components/AccountStats";
 import History from "./components/History";
 import Navbar from "./components/Navbar";
-import { useEffect, useState } from "react";
-import { formatter } from "./helper/balanceFormatter";
 
 function App() {
   return (
